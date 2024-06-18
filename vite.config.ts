d812655fb7cf366c5a3ce16a5b9b27b4ca78ef89
Ivan Vrublevsky <wrublik.93@mail.ru>
2024-06-19 01:52:06 +0300

    init: added netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs'
-  },
+  // build: {
+  //   outDir: './docs'
+  // },
 });
