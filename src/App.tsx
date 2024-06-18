4d2d587c2b4793b025cf5c3a01410c107fc951c4
Ivan Vrublevsky <wrublik.93@mail.ru>
2024-06-19 02:39:22 +0300

    init: added tma SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import '@telegram-apps/telegram-ui/dist/styles.css';
 
-import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
+// import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
+import {SDKProvider} from "@tma.js/sdk-react";
 
-import "./App.css";
+// import "./App.css";
 
 // import WebApp from "@twa-dev/sdk";
 
 export const App = () => {
   return (
-    <AppRoot>
-      <Placeholder
-        header="Title"
-        description="Description"
-      >
-        <img
-          alt="Telegram sticker"
-          src="https://xelene.me/telegram.gif"
-          style={{ display: 'block', width: '144px', height: '144px' }}
-        />
-      </Placeholder>
-    </AppRoot>
+    <SDKProvider acceptCustomStyles>
+      Hello!
+    </SDKProvider>
   );
 };
